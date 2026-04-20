74912481d31d87c87ad6ee82d877ecdec6f34445
Pamerla Rithin <157945399+rithinsai-dev@users.noreply.github.com>
2026-04-20 10:46:36 +0530

    intialised the backend and frontend

---
+++ mockhackathon-backend/src/main/java/com/mockhackathon/mockhackathon/MockhackathonApplication.java
@@ -0,0 +1,13 @@
+package com.mockhackathon.mockhackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockhackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockhackathonApplication.class, args);
+	}
+
+}
